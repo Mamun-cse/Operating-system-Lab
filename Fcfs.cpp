b7a61201e7da2acae2f794902daf8d2e76d61165
Mamun-cse <37404399+Mamun-cse@users.noreply.github.com>
2019-11-30 13:55:09 -0400

    Completed

--- Fcfs.cpp
+++ Fcfs.cpp
@@ -1,74 +1,44 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+void findWaitingTime(int pro[],int n,int B[]){
+    int tn[n],wt[n],i,totalTn=0,totalWt=0;
+    tn[0]=0;
+    wt[0]=0;
 
-// Function to find the waiting time for all
-// processes
-void findWaitingTime(int processes[], int n,
-                          int bt[], int wt[])
-{
-    // waiting time for first process is 0
-    wt[0] = 0;
+    for(i=1;i<=n;i++){
+        tn[i] = tn[i-1]+B[i];
+        totalTn = totalTn + tn[i];
+    }
 
-    // calculating waiting time
-    for (int  i = 1; i < n ; i++ )
-        wt[i] =  bt[i-1] + wt[i-1] ;
-}
+    for(i=1;i<=n;i++){
+        wt[i] = (tn[i]-B[i]);
+        totalWt = totalWt + wt[i];
+    }
 
-// Function to calculate turn around time
-void findTurnAroundTime( int processes[], int n,
-                  int bt[], int wt[], int tat[])
-{
-    // calculating turnaround time by adding
-    // bt[i] + wt[i]
-    for (int  i = 0; i < n ; i++)
-        tat[i] = bt[i] + wt[i];
-}
-
-//Function to calculate average time
-void findavgTime( int processes[], int n, int bt[])
-{
-    int wt[n], tat[n], total_wt = 0, total_tat = 0;
-
-    //Function to find waiting time of all processes
-    findWaitingTime(processes, n, bt, wt);
-
-    //Function to find turn around time for all processes
-    findTurnAroundTime(processes, n, bt, wt, tat);
-
-    //Display processes along with all details
     cout << "Processes  "<< " Burst time  "
          << " Waiting time  " << " Turn around time\n";
-
-    // Calculate total waiting time and total turn
-    // around time
-    for (int  i=0; i<n; i++)
-    {
-        total_wt = total_wt + wt[i];
-        total_tat = total_tat + tat[i];
-        cout << "   " << i+1 << "\t\t" << bt[i] <<"\t    "
-            << wt[i] <<"\t\t  " << tat[i] <<endl;
-    }
-
+    for(i=1;i<=n;i++){
+        cout<<" "<<pro[i]<<"\t\t"<<B[i]<<"\t\t"
+            <<wt[i]<<"\t\t"<<tn[i]<<endl;
+         }
+    cout<<endl;
     cout << "Average waiting time = "
-         << (float)total_wt / (float)n;
-    cout << "\nAverage turn around time = "
-         << (float)total_tat / (float)n;
-}
+         << (float)totalWt / (float)n;
 
-// Driver code
+    cout << "\nAverage turn around time = "
+         << (float)totalTn / (float)n;
+}
 int main()
 {
-    int n;
-    cout<<"enter the number of process\n";
+    int n,i;
+    cout<<"Total number of process\n";
     cin>>n;
-    int processes[n] ,burst_time[n];
-    cout<<"Enter The Burst time\n";
-    for(int i=0;i<n;i++){
-        cin>>processes[i];
-        cin>>burst_time[i];
-    }
-    findavgTime(processes, n,  burst_time);
-    return 0;
+    int p[n],b[n];
+    cout<<"processes "<<"burst time\n";
+     for(i=1;i<=n;i++){
+        cin>>p[i];
+        cin>>b[i];
+     }
+     findWaitingTime(p,n,b);
 }
-
