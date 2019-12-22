e2d50033f4e98ed9ada7b38badc8fed9d014f29f
Mamun-cse <37404399+Mamun-cse@users.noreply.github.com>
2019-12-21 23:16:06 -0800

    Bankers Deadlock Avoidance Algorithm

---
+++ BankersAlgo.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m, i, j, k;
+    n = 5;
+    m = 3;
+    int alloc[n][m],max[n][m],avail[m],temp[m]={0},instance[m]={0};
+    cout<<"Enter instance\n";
+    for(i=0;i<m;i++){
+        cin>>instance[i];
+    }
+
+    cout<<"Enter allocation\n";
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            cin>>alloc[i][j];
+        }
+    }
+
+    cout<<"Enter maximum\n";
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            cin>>max[i][j];
+        }
+    }
+
+
+    /*cout<<"Enter available\n";
+    for(i=0;i<m;i++){
+        cin>>avail[i];
+    }*/
+
+    for(int i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            temp[i]=temp[i]+alloc[j][i];
+        }
+        avail[i]=instance[i]-temp[i];
+    }
+
+
+    int f[n], ans[n], ind = 0;
+    for (k = 0; k < n; k++) {
+        f[k] = 0;
+    }
+    int need[n][m];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+    }
+    int y = 0;
+    for (k = 0; k < 5; k++) {
+        for (i = 0; i < n; i++) {
+            if (f[i] == 0) {
+
+                int flag = 0;
+                for (j = 0; j < m; j++) {
+                    if (need[i][j] > avail[j]){
+                        flag = 1;
+                         break;
+                    }
+                }
+
+                if (flag == 0) {
+                    ans[ind++] = i;
+                    for (y = 0; y < m; y++)
+                        avail[y] += alloc[i][y];
+                    f[i] = 1;
+                }
+            }
+        }
+    }
+
+    printf("Following is the SAFE Sequence\n");
+    for (i = 0; i < n - 1; i++)
+        printf(" P%d ->", ans[i]);
+    printf(" P%d", ans[n - 1]);
+
+    return (0);
+}
